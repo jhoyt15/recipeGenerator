5bd46ea7edfbc818366da9fe9800a8fc554e577a
unknown <jacksonhoyt26@gmail.com>
2023-09-25 19:01:16 -0400

    Created intial version of password form

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import dataHandling.dbInit as dbInit
 import dataHandling.recipeDataHandler as recipeDataHandler
 import dataHandling.generateRecipe as generateRecipe
+from GUI.PasswordWindow import PasswordWindow
+from PyQt5.QtWidgets import QApplication
+import sys
 
-password = input("Input Password: ")
-credentials = {"host":"localhost","user":"root","password":password,"database":"recipeDatabase"}
-database = recipeDataHandler.RecipeDataHandler(credentials)
-recipeList = generateRecipe.generateRecipe(["beef","cheese"],database)
-print(recipeList)
+#password = input("Input Password: ")
+#credentials = {"host":"localhost","user":"root","password":password,"database":"recipeDatabase"}
+#database = recipeDataHandler.RecipeDataHandler(credentials)
+#recipeList = generateRecipe.generateRecipe(["beef","cheese"],database)
+#print(recipeList)
+
+QApp = QApplication(sys.argv)
+application = PasswordWindow()
+application.createUI()
+sys.exit(QApp.exec_())
