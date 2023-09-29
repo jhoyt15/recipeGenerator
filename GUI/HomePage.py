91d5d90a89c97bcd96fccb131e6ccd928c83ccac
unknown <jacksonhoyt26@gmail.com>
2023-09-28 20:07:12 -0400

    Added ability to add ingredients and display selected ingredients

--- GUI/HomePage.py
+++ GUI/HomePage.py
@@ -5,11 +5,12 @@ from PyQt5.QtCore import Qt
 
 class HomePage(QMainWindow):
     ingredientList = [] #List of all ingredients that the user has entered
+    ingredientListWidgets = []
 
     def __init__(self):
         super(HomePage,self).__init__()
         self.setWindowTitle("Recipe Generator")
-        self.setWindowIcon(QIcon("GUI/HomePageIcon.jpg"))
+        self.setWindowIcon(QIcon("GUI/Images/HomePageIcon.jpg"))
         with open("GUI/Styles/HomePage.css","r") as file:
             self.setStyleSheet(file.read())
     
@@ -25,6 +26,8 @@ class HomePage(QMainWindow):
 
         self.createAddIngredientButton()
 
+        self.addSelectedIngredientsSection()
+
         topWidget = QtWidgets.QWidget()
         topWidget.setLayout(self.topLayout)
         self.setCentralWidget(topWidget)
@@ -35,7 +38,7 @@ class HomePage(QMainWindow):
     def createImage(self) -> None:
         self.imageLabel = QtWidgets.QLabel() #Label that will hold the image
 
-        self.imageMap = QPixmap("GUI/HomePageImage.jpg") #Pixmap of the image
+        self.imageMap = QPixmap("GUI/Images/mainLogo.png") #Pixmap of the image
 
         self.imageLabel.setPixmap(self.imageMap) #Set the pixmap of the label
 
@@ -61,7 +64,7 @@ class HomePage(QMainWindow):
 
     def createAddIngredientButton(self) -> None:
         self.addIngredientButton = QtWidgets.QPushButton("Add Ingredient")
-        self.addIngredientButtonIcon = QIcon("GUI/plusIcon.png")
+        self.addIngredientButtonIcon = QIcon("GUI/Images/plusIcon.png")
         self.addIngredientButton.setIcon(self.addIngredientButtonIcon)
         self.addIngredientButton.setObjectName("addIngredientButton")
         self.addIngredientButton.clicked.connect(self.addIngredient)
@@ -71,13 +74,31 @@ class HomePage(QMainWindow):
 
     #This function will add the ingredient to the ingredients list
     def addIngredient(self) -> None:
-        print("Clicked")
-        self.ingredientList.append(self.ingredientField.text())
-        print(self.ingredientList)
+        text = self.ingredientField.text()
+        if text != "": 
+            self.ingredientList.append(text)
+            ingredientName = QLabel(text.capitalize())
+            deleteButton = QPushButton()
+            deleteButton.setIcon(QIcon("GUI/Images/deleteIcon.png"))
+            deleteButton.clicked.connect(self.deleteIngredient)
+            self.ingredientListWidgets.append(deleteButton)
+            self.ingredientsSelectedLayout.addRow(ingredientName,deleteButton)
+
+    def deleteIngredient(self):
+        button = self.sender()
+        self.ingredientsSelectedLayout.removeRow(button)
+        self.ingredientListWidgets.remove(button)
 
     #This function will initialize and add the list of selected ingredients
     def addSelectedIngredientsSection(self):
-        pass
+        self.ingredientsSelected = QGroupBox("Selected Ingredients") #Create the group box to hold all the ingredients
+        self.ingredientsSelected.setFixedHeight(200)
+
+        self.ingredientsSelectedLayout = QFormLayout() #Main layout for the group box
+
+        self.ingredientsSelected.setLayout(self.ingredientsSelectedLayout)
+
+        self.topLayout.addWidget(self.ingredientsSelected)
 
 
     def ingredientRecommend(self) -> None:
